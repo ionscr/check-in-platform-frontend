65183d2281e919e3c60429456778b4824d85d0c6
Ion Scripnic <asdfree03@gmail.com>
2021-07-28 00:41:14 +0300

    created classroom model

---
+++ src/app/models/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id?: number;
+    first_name: string;
+    last_name: string;
+    role: number;
+    year?: number;
+    department?: string;
+    section?: string;
+    f_group?: string;
+  }

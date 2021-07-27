c9edf2f3d66c1c63bbcbc3a8dfa62ef66f45faf1
Ion Scripnic <asdfree03@gmail.com>
2021-07-28 00:34:40 +0300

    created user model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Check-in Application';
+  title: string = 'Check-in Platform';
 }

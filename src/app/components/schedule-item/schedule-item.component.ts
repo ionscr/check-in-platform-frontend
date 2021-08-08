25d7884fba545b947b29342117dc6380da3aeff2
Ion Scripnic <asdfree03@gmail.com>
2021-08-08 17:39:03 +0300

    added teacher modal

--- src/app/components/schedule-item/schedule-item.component.ts
+++ src/app/components/schedule-item/schedule-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Reservations } from 'src/app/models/Reservations';
 import { Schedule } from 'src/app/models/Schedule';
 import { User } from 'src/app/models/User';
@@ -8,7 +8,7 @@ import { RoleService } from 'src/app/services/role.service';
 import { UserService } from 'src/app/services/user.service';
 
 import { map, filter } from 'rxjs/operators';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { TemplateRef } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -25,6 +25,7 @@ export class ScheduleItemComponent implements OnInit {
   students: User[] = [];
   capacity: number = 0;
   faTimes = faTimes;
+  studentOk: number = 1;
   constructor(private reservationsService: ReservationsService, private modalService: NgbModal, private roleService: RoleService, private userService: UserService) { }
 
   ngOnInit(): void {
@@ -40,9 +41,18 @@ export class ScheduleItemComponent implements OnInit {
     this.userService.findUsersByRole(1).subscribe((students) => (this.students = students));
   }
   addReservation(student: User): void{
+    this.studentOk = 1;
     const student1: User = {id: Number(JSON.stringify(student).match(/\d/g)), first_name: "", last_name: "", role: 1};
-    const reservation: Reservations = {schedule: this.dayClass, student: student1}
-    this.reservationsService.addReservation(reservation).subscribe((reservation) => (this.reservations.push(reservation), this.capacity = this.reservations.length));
+    this.reservations.forEach(reservation => {
+      if(reservation.student.id === student1.id) this.studentOk = 0;
+    });
+    if(this.studentOk){
+      const reservation: Reservations = {schedule: this.dayClass, student: student1}
+      this.reservationsService.addReservation(reservation).subscribe((reservation) => (this.reservations.push(reservation), this.capacity = this.reservations.length));
+    }
+    else{
+      alert("This student already has a reservation!");
+    }
   }
   openModal(content: TemplateRef<any>): void {
     this.role = this.roleService.getRole();
